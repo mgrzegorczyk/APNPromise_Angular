40f740c58e4459dcad1130ba09b5a29f4d52080a
mgrzegor <m-grzegorczyk@o2.pl>
2024-06-20 00:09:17 +0200

    currency list - spinner refactor

--- src/app/features/currency/currency-list/currency-list.component.ts
+++ src/app/features/currency/currency-list/currency-list.component.ts
@@ -10,7 +10,6 @@ import {CurrencyService} from "../services/currency.service";
 export class CurrencyListComponent implements OnInit {
 
   rates$!: Observable<any[]>;
-  isLoading$!: Observable<boolean>;
   date: string = '';
   maxDate: Date = new Date();
   today: Date = new Date();
@@ -19,7 +18,6 @@ export class CurrencyListComponent implements OnInit {
 
   ngOnInit(): void {
     this.date = this.today.toISOString().split('T')[0];
-    this.isLoading$ = this.currencyService.loading$;
     this.getRates();
   }
 
--- src/app/features/currency/services/currency.service.ts
+++ src/app/features/currency/services/currency.service.ts
@@ -1,32 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, BehaviorSubject } from 'rxjs';
-import { map, catchError, finalize, tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class CurrencyService {
 
   private API_URL = 'https://api.nbp.pl/api/exchangerates/tables/A/';
-  private supportedCurrencies = ['EUR', 'USD', 'PLN', 'CHF', 'GBP'];
-  private loadingSubject = new BehaviorSubject<boolean>(false);
-  public loading$ = this.loadingSubject.asObservable();
+  private supportedCurrencies = ['EUR', 'USD', 'CHF', 'GBP'];
 
   constructor(private http: HttpClient) { }
 
   getExchangeRates(date: string = ''): Observable<any[]> {
-    const formattedDate = date ? new Date(date).toISOString().split('T')[0] : '';
-    const url = formattedDate ? `${this.API_URL}${formattedDate}/` : this.API_URL;
-    this.loadingSubject.next(false);
+    const url = date ? `${this.API_URL}${date}/` : this.API_URL;
     return this.http.get<any[]>(url).pipe(
-      tap(() => this.loadingSubject.next(false)),
-      map(response => response[0].rates.filter((rate: { code: string; }) => this.supportedCurrencies.includes(rate.code))),
-      catchError(err => {
-        this.loadingSubject.next(false);
-        throw err;
-      }),
-      finalize(() => this.loadingSubject.next(false))
+      map(response => {
+        return response[0].rates.filter((rate: { code: string; }) => {
+          return this.supportedCurrencies.includes(rate.code);
+        });
+      })
     );
   }
 }
